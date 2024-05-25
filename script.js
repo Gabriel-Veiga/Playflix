85ad9d87c2a4a24d4ba219100537a6967d06b1a3
Gabriel Veiga <142056149+Gabriel-Veiga@users.noreply.github.com>
2024-05-24 21:27:09 -0300

    Playflix

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.gallery img').forEach(img => {
+    const originalSrc = img.src;
+    const hoverSrc = img.getAttribute('data-hover');
+    img.addEventListener('mouseover', () => {
+        img.src = hoverSrc;
+    });
+
+    img.addEventListener('mouseout', () => {
+        img.src = originalSrc;
+});
+});
